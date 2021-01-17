a7f3cb373c55ce76ca72306a6c997dbbd1169543
nimcurry <nimcurry@yahoo.co.in>
2021-01-17 15:53:34 -0500

    changed settings file

--- carwebsite/settings.py
+++ carwebsite/settings.py
@@ -176,4 +176,5 @@ EMAIL_USE_TLS = True
 
 #whitenoise settings
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
